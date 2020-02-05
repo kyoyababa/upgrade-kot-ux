3e9ff8d6270fa77af244300850d33497b69766d5
kyoyababa <amour.le.jour.ideal@gmail.com>
2020-02-05 22:42:03 +0900

    completed the program

--- workspace/assets/scripts/upgradeKotUx__content.js
+++ workspace/assets/scripts/upgradeKotUx__content.js
@@ -5,18 +5,92 @@ window.$ = window.jQuery = jQuery;
 
 class UpgradeKotUx {
   constructor() {
-    if (!this.checkIfKotPageIsCorrect()) return;
+    if (this.checkIfKotPageIsCorrect('top')) {
+      return this.activateKantanButton();
+    }
 
+    if (!this.checkIfKotPageIsCorrect('dakokuShinsei')) {
+      return;
+    }
+
+    if (this.isAlreadyArrived()) {
+      this.doTaikinDakoku();
+    } else {
+      this.doShukkinDakoku();
+    }
+  }
+
+  activateKantanButton() {
     const $todayRow = this.findTodayRow();
     if ($todayRow.length === 0) return;
 
-    // TODO(baba): こいつをクリックしたことにしたい
-    // $todayRow.find('.htBlock-selectOther > option')[1]
+    $('body').append(`
+      <div id="kantan-dakoku-shinsei">
+        かんたん打刻申請
+        <style>
+          #kantan-dakoku-shinsei {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+            background-color: red;
+            color: #fff;
+            font-size: 24px;
+            font-weight: bold;
+            line-height: 1;
+            padding: 20px;
+            border-radius: 4px;
+            box-shadow: 0 3px 6px #666;
+            cursor: pointer;
+            z-index: 9999;
+          }
+        </style>
+      </div>
+    `);
+
+    $('#kantan-dakoku-shinsei').click(() => {
+      this.moveToDakokuShinseiPage($todayRow);
+    });
   }
 
-  checkIfKotPageIsCorrect() {
-    const $targetTableWrapper = $('.htBlock-adjastableTableF_inner');
-    return $targetTableWrapper.length === 1;
+  doShukkinDakoku() {
+    this.doDakoku('1');
+  }
+
+  doTaikinDakoku() {
+    this.doDakoku('2');
+  }
+
+  doDakoku(dakokuValue) {
+    const $shukkinSelection = $('select[name=recording_type_code_1]');
+    $shukkinSelection.val(dakokuValue);
+
+    const $shukkinTime = $('#recording_timestamp_time_1');
+    const currentTime = new Date();
+    const currentTimeText = `00${currentTime.getHours()}`.slice(-2) + ':' + `00${currentTime.getMinutes()}`.slice(-2);
+    $shukkinTime.val(currentTimeText);
+
+    const $cta = $('#button_01');
+    $cta.click();
+  }
+
+  isAlreadyArrived() {
+    const $dakokuHistoryTable = $('.specific-table_1000_wrap');
+    return $dakokuHistoryTable.length === 1;
+  }
+
+  moveToDakokuShinseiPage($todayRow) {
+    const todayTriggerId = $todayRow.find('.htBlock-selectOther > option').eq(1).val();
+    $(todayTriggerId).click();
+  }
+
+  checkIfKotPageIsCorrect(pagePattern) {
+    if (pagePattern === 'top') {
+      const $targetTableWrapper = $('.htBlock-adjastableTableF_inner');
+      return $targetTableWrapper.length === 1;
+    } else if (pagePattern === 'dakokuShinsei') {
+      const $targetTableWrapper = $('#recording_timestamp_table');
+      return $targetTableWrapper.length === 1;
+    }
   }
 
   findTodayRow() {
